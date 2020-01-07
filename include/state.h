81fe67547c6c2dabbb1903150a19cea7119bffa4
Byunggul Koh <byunggul.koh@lge.com>
2020-01-07 16:13:42 +0900

    Allow user to set environment variable
    Change-Id: I3801c51cdcf746c5d294b51afdd7a5eb92302566

--- include/state.h
+++ include/state.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2019 LG Electronics, Inc.
+// Copyright (c) 2019-2020 LG Electronics, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -35,6 +35,7 @@ public:
 
     void setEnvVar(DFAEnv* env) { this->env = env; }
     void setOutput(TimedBlockList<std::pair<T, std::chrono::milliseconds>>* output) { this->output = output; }
+    DFAEnv* getEnvVar() { return this->env; }
 
 protected:
     // When you use a pointer as environement variable,
--- src/discriminator.hpp
+++ src/discriminator.hpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2019 LG Electronics, Inc.
+// Copyright (c) 2019-2020 LG Electronics, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -71,7 +71,12 @@ template<typename Input>
 void Discriminator<Input>::addInitState(State<Input>* state) {
     // Environment variables are reused on the whole life
     // cycle of the state chains.
-    state->setEnvVar((env[state] = new DFAEnv()));
+    auto envVar = state->getEnvVar();
+    if (!envVar) {
+        state->setEnvVar((env[state] = new DFAEnv()));
+    } else { // User can set their Environemnt variable
+        env[state] = envVar;
+    }
     state->setOutput(&outputList);
     m_initialStates.push_back(state);
 }
